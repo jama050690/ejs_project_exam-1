28aa2e741096c0630dd5c8eecab93f64b5f9ff9e
Jamshiddin <jbm050690@gmail.com>
2026-01-11 16:34:50 +0500

    favicon qo'ydim

--- src/server.js
+++ src/server.js
@@ -11,13 +11,20 @@ export function main() {
   console.log(__dirname);
 
   const app = express();
-  app.use(express.static(path.join(__dirname, "../public")));
 
   app.set("view engine", "ejs");
   app.set("views", path.join(__dirname, "views"));
+  app.use(express.static(path.join(__dirname, "../public")));
+
   app.get("/", (req, res) => {
     res.render("index", {
       title: "Dashboard",
+    });
+  });
+  app.use((req, res, next) => {
+    res.status(404).render("404", {
+      title: " Error 404",
+      text: "Page Not Found",
     });
   });
 
